2ecb0a989bccce65a5e5468c0adac34f9d3d215d
fangtingkuo <D0988723@o365.fcu.edu.tw>
2023-06-13 13:59:59 +0800

    Initial Commit

---
+++ story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  story
+//
+//  Created by 郭芳廷 on 2023/6/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
